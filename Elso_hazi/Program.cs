cbdfc02960441114aadfbe0d35cccb3996ca6365
Anikó <nemcsa3@gmail.com>
2019-09-27 13:07:51 +0200

    1.Feladat

--- Elso_hazi/Program.cs
+++ Elso_hazi/Program.cs
@@ -10,6 +10,18 @@ namespace Elso_hazi
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Adjon meg 10 számot!");
+            List<int> szamokLista = new List<int>();  
+            for (int i = 0; i < 10; i++)
+            {
+                szamokLista.Add(Convert.ToInt32(Console.ReadLine()));
+            }
+
+            int max = szamokLista.Max();
+            Console.WriteLine("A legnagyobb szám: {0}", max);
+
+
+            Console.ReadKey();
         }
     }
 }
