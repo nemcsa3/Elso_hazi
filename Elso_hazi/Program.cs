31e6228da9f3d89d86ec009dfd483da0eeb5922e
nemcsa3 <nemcsa3@gmail.com>
2019-09-29 14:25:04 +0200

    4.Feladat Házi vége

--- Elso_hazi/Program.cs
+++ Elso_hazi/Program.cs
@@ -10,22 +10,27 @@ namespace Elso_hazi
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Adja meg, hány számot szeretne beírni!");
-            int db = Convert.ToInt32(Console.ReadLine());
-            
-
-            Console.WriteLine("Adja meg a számokat!");
-            List<int> szamokLista = new List<int>();  
-            for (int i = 0; i < db; i++)
-            {
-                szamokLista.Add(Convert.ToInt32(Console.ReadLine()));
-            }
-
-            int max = szamokLista.Max();
-            int min = szamokLista.Min();
-            Console.WriteLine("A legnagyobb szám: {0}\nA legkisebb szám: {1}", max, min);
-
-
+            try 
+	        {	        
+		        Console.WriteLine("Adja meg, hány számot szeretne beírni!");
+                int db = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Adja meg a számokat!");
+                List<int> szamokLista = new List<int>();  
+                for (int i = 0; i < db; i++)
+                {
+                    szamokLista.Add(Convert.ToInt32(Console.ReadLine()));
+                }
+                int max = szamokLista.Max();
+                int min = szamokLista.Min();
+                Console.WriteLine("A legnagyobb szám: {0}\nA legkisebb szám: {1}", max, min);
+	        }
+	        catch (Exception)
+	        {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Hiba történt!\n" +
+                    "Nem egész vagy túl nagy egész számot adtál meg, a program emiatt leáll!");	
+                Console.ResetColor();
+	        }
             Console.ReadKey();
         }
     }
